f35c03a65095067c173706932e247f50d6a9fede
jose <jose.correa-r@mail.escuelaing.edu.co>
2024-02-07 07:53:10 -0500

    rescribir

--- mi-primera-app/src/main/java/edu/escuelaing/arem/ASE/tallerCasa/HttpServer.java
+++ mi-primera-app/src/main/java/edu/escuelaing/arem/ASE/tallerCasa/HttpServer.java
@@ -1,43 +1,29 @@
 package edu.escuelaing.arem.ASE.tallerCasa;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import org.json.JSONObject;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
+import java.net.*;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class HttpServer {
-       private static final int PORT = 35000;
-    private static final HttpApi apf = new HttpApi();
-
-    /**
-     * Defautl Constructor
-     */
-    public HttpServer() {
-
-    }
-
- 
-    public static void startSever() throws IOException {
+    
+    private static HTTPMovie apiConection = new HTTPMovie();
+    private static ConcurrentHashMap<String,String> cache = new ConcurrentHashMap<>();
+    
+    private static HashMap<String,Object> Data = new HashMap<>();
+    public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = null;
-
         try {
-            serverSocket = new ServerSocket(PORT);
+            serverSocket = new ServerSocket(35000);
         } catch (IOException e) {
             System.err.println("Could not listen on port: 35000.");
             System.exit(1);
         }
-
         boolean running = true;
-        while (running) {
+        while (running){
             Socket clientSocket = null;
             try {
                 System.out.println("Listo para recibir ...");
@@ -46,100 +32,179 @@ public class HttpServer {
                 System.err.println("Accept failed.");
                 System.exit(1);
             }
+
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            BufferedReader in = new BufferedReader(
+            new InputStreamReader(clientSocket.getInputStream()));
             String inputLine, outputLine;
-            boolean readingFirst = true;
-            String petition = "";
-
+            boolean fLine = true;
+            String uriS = "";
             while ((inputLine = in.readLine()) != null) {
-
-                if (readingFirst) {
-                    petition = inputLine.split(" ")[1];
-                    readingFirst = false;
+                
+                System.out.println("Received: " + inputLine);
+                if (fLine) {
+                    fLine = false;
+                    uriS = inputLine.split(" ")[1];
                 }
                 if (!in.ready()) {
                     break;
                 }
             }
-
-            System.out.println("ASi llego la peticion: " + petition);
-
-            outputLine = (petition.startsWith("/movie")) ? movieInfo(petition.replace("/movie?name=", ""))
-                    : mainPage(petition);
-
+            if (uriS.startsWith("/movie?")) {
+                outputLine = getMovie(uriS);
+            }else if(uriS.startsWith("/movie?name=Close") || uriS.startsWith("/moviepost?name=Close")){
+                running = false;
+                outputLine = getIndexResponse();
+            }
+            else {
+                outputLine = getIndexResponse();
+            }
             out.println(outputLine);
             out.close();
             in.close();
             clientSocket.close();
         }
-
         serverSocket.close();
     }
-
-  
-    private static String movieInfo(String name) {
-        try {
-
-            JsonObject resp = apf.searchMovie(name);
-            JsonElement title = resp.get("Title"), poster = resp.get("Poster"), director = resp.get("Director"),
-                    plot = resp.get("Plot");
-
-            String outputLine = "HTTP/1.1 200 OK\r\n"
-                    + "Content-Type:text/html\r\n"
+            
+    public static String getMovie(String uri) {
+        String name = nameFixer(uri.split("=")[1]);
+        String message;
+        String httpList;
+        if (cache.containsKey(name.toUpperCase())){
+            message = cache.get(name.toUpperCase());
+            movieSetter(message);
+            httpList = info();
+            return "HTTP/1.1 200 OK\r\n"
+                    + "Content-Type:  text/html\r\n"
                     + "\r\n"
-                    + getStaticFile("/movieInfo.html").replace("{Title}", title.toString())
-                            .replace("\"{Poster}\"", poster.toString()).replace("{Directors}", director.toString())
-                            .replace("{Plot}", plot.toString());
-
-            return outputLine;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-  
-    private static String mainPage(String filePetition) {
-
-        String mimeType = getMimeType(filePetition);
-        System.out.println("mimetype regresado para " + filePetition + ": " + mimeType);
-        String resp = getStaticFile(filePetition);
-
-        String outputLine = "HTTP/1.1 200 OK\r\n"
-                + "Content-Type:" + mimeType + "\r\n"
-                + "\r\n"
-                + resp;
-
-        return outputLine;
-    }
-
-  
-    private static String getMimeType(String filePetition) {
-        return (filePetition.endsWith(".html") || filePetition.endsWith("/")) ? "text/html"
-                : ((filePetition.endsWith(".css")) ? "text/css"
-                        : (filePetition.endsWith(".js")) ? "application/javascript" : (filePetition.endsWith(".jpg")) ? "image/jp2" : "text/plain");
-    }
-
- 
-    private static String getStaticFile(String filePetition) {
-        Path file = (filePetition.equals("/")) ? Paths.get("target/classes/public/index.html")
-                : Paths.get("target/classes/public" + filePetition);
-
-        Charset charset = Charset.forName("UTF-8");
-        StringBuilder outputLine = new StringBuilder();
-        try (BufferedReader reader = Files.newBufferedReader(file, charset)) {
-            String line = null;
-
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-                outputLine.append(line).append("\n");
+                    + "<!DOCTYPE html>\n"
+                    + "<html>\n"
+                    + " <head>\n"
+                    + "     <meta charset=\"UTF-8\">\n"
+                    + "     <title>List</title> \n"
+                    + " </head>\n"
+                    + " <body>\n"
+                    + "     " + httpList
+                    + " </body>\n"
+                    +"</html>";
+        }else{
+            apiConection.movieSet(name);
+            try {
+                apiConection.run();
+                message = apiConection.getMessage();
+                movieSetter(message);
+                httpList = info();
+                cache.put(name.toUpperCase(),message);
+                return  "HTTP/1.1 200 OK\r\n"
+                        + "Content-Type:  text/html\r\n"
+                        + "\r\n"
+                        + "<!DOCTYPE html>\n"
+                        + "<html>\n"
+                        + " <head>\n"
+                        + "     <meta charset=\"UTF-8\">\n"
+                        + "     <title>List</title> \n"
+                        + " </head>\n"
+                        + " <body>\n"
+                        + "     " + httpList
+                        + " </body>\n"
+                        +"</html>";
+            }catch (IOException x){
+                x.printStackTrace();
             }
-        } catch (Exception e) {
-            System.err.format("IOException: "+ e.getMessage(), e);
         }
-
-        return outputLine.toString();
+        return "Error 404";
+    }   
+    
+        /**
+     * This method will extract the values of the dictionary where is located the json message
+     * @return the part in html listing the data from the movie that is wnat to be show
+     */
+    public static String info(){
+        String title = (String) Data.get("Title");
+        String year = (String) Data.get("Year");
+        String genre = (String) Data.get("Genre");
+        String director = (String) Data.get("Director");
+        String sinopsis = (String) Data.get("Plot");
+        return "<ul>\n"
+                +"  <li> Title: " + title + "</li>\n"
+                +"  <li> Year: " + year + "</li>\n"
+                +"  <li> Genre: " + genre + "</li>\n"
+                +"  <li> Director: " + director + "</li>\n"
+                + " <li> Sinopsis: " + sinopsis + "</li>\n"
+                +"</ul>\n";
     }
+            
+    public static String getIndexResponse() {
+        return "HTTP/1.1 200 OK\r\n"
+                + "Content-Type: text/html\r\n"
+                + "\r\n"
+                + "<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "    <head>\n" +
+                "        <title>Movie Search </title>\n" +
+                "        <meta charset=\"UTF-8\">\n" +
+                "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+                "       <style>\n" +
+                "           body{\n" +
+                "           label, input[type=\"text\"],input[type=\"button\"]{\n" +
+                "           }"+
+                "       </style>" +
+                "    </head>\n" +
+                "    <body>\n" +
+                "        <h1>MOVIE SEARCH </h1>\n" +
+                "        <form action=\"/movie\">\n" +
+                "            <label for=\"name\">Name:</label><br>\n" +
+                "            <input type=\"text\" id=\"name\" name=\"name\" value=\"Guardians of the galaxy\"><br><br>\n" +
+                "            <input type=\"button\" value=\"Submit\" onclick=\"loadGetMsg()\">\n" +
+                "        </form> \n" +
+                "        <div id=\"getrespmsg\"></div>\n" +
+                "\n" +
+                "        <script>\n" +
+                "            function loadGetMsg() {\n" +
+                "                let nameVar = document.getElementById(\"name\").value;\n" +
+                "                const xhttp = new XMLHttpRequest();\n" +
+                "                xhttp.onload = function() {\n" +
+                "                    document.getElementById(\"getrespmsg\").innerHTML =\n" +
+                "                    this.responseText;\n" +
+                "                }\n" +
+                "                xhttp.open(\"GET\", \"/movie?name=\"+nameVar);\n" +
+                "                xhttp.send();\n" +
+                "            }\n" +
+                "        </script>\n" +
+                "\n" +
+                "        </script>\n" +
+                "    </body>\n" +
+                "</html>";
+    }
+    
+     public static String nameFixer(String uriName){
+        return uriName.replace("%20", " ");
+    }
+
+    /**
+     * This method have the mission of mapping a json intro a Hashmap
+     * @param jsonString Should be the String version of the JSON
+     */
+    public static void movieSetter(String jsonString){
+        try{
+            JSONObject jsonObj = new JSONObject(jsonString);
+            Iterator<String> keys = jsonObj.keys();
+            while (keys.hasNext()){
+                String key = keys.next();
+                Object value = jsonObj.get(key);
+                Data.put(key,value);
+            }
+        }catch (Exception x){
+            x.printStackTrace();
+        }
+    }
+
+    public static HashMap<String, Object> getMovieData() {
+        return Data;
+    }
+
+
+
 }
+
